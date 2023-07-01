1ead44f399efd6fdcda4ed65dae16fd8220b9ace
Devin <devincb93@gmail.com>
2023-07-01 14:58:57 -0700

    Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 2
+const num2 = 31
+const num3 = 6
+const num4 = 28
+let multiply = num1 * num2;
+let random = Math.ceil(Math.random() * 10);
+let mod = num4 % num3;
+let max = Math.ceil(20);
